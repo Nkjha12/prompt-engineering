15a55a8b0f1c259750ca5f02034089a97c18c1df
Nkjha12 <nitinjha@Nitins-MacBook-Air.local>
2025-07-09 13:37:01 +0530

    Add Module 11: Prompt Templates

--- prompt-env/bin/activate.fish
+++ prompt-env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nitinjha/Desktop/Week6/Prompt-Emgieering/prompt-env"
+set -gx VIRTUAL_ENV "/Users/nitinjha/Desktop/Week6/Prompt-Engieering/prompt-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
